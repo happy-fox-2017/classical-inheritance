f75110fc488eb2b4d973e83fa5f8beea5d2fa8b5
myyusuf <myyusuf1911@gmail.com>
2017-05-30 08:09:07 +0700

    Release 1: Super powered animals

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,42 +1,68 @@
 "use strict"
 
-"use strict"
+class SuperPower {
+  useLaserVision() {
+    console.log('Using laser vision!...');
+  }
+
+  beInvisible() {
+    console.log('Become invisible...');
+  }
+}
 
 class Animal {
 
-  constructor(numLegs = 2, isWarmBlooded) {
-    this.numLegs = numLegs;
+  constructor(isWarmBlooded, numLegs = 2) {
     this.isWarmBlooded = isWarmBlooded;
+    this.numLegs = numLegs;
+    this.superPower = new SuperPower();
   }
 
 }
 
 class Frog extends Animal {
   constructor() {
-    super(4, false);
+    super(false, 4);
   }
 }
 
 class Bat extends Animal {
   constructor() {
-    super(2, true);
+    super(true);
   }
 }
 
 class Chimpanzee extends Animal {
   constructor() {
-    super(2, true);
+    super(true);
   }
 }
 
 class Fox extends Animal {
   constructor() {
-    super(4, true);
+    super(true, 4);
   }
 }
 
 class Chicken extends Animal {
   constructor() {
-    super(2, true);
+    super(true);
   }
 }
+
+// Driver Code :
+
+const frog = new Frog();
+const bat = new Bat();
+const chimpanzee = new Chimpanzee();
+const fox = new Fox();
+const chicken = new Chicken();
+
+const animals = [frog, bat, chimpanzee, fox, chicken];
+animals.forEach((animal) => {
+  console.log('-----------------------------------');
+  console.log(`${animal.constructor.name} ${animal.isWarmBlooded ? 'warm blooded' : 'cold blooded'} with ${animal.numLegs} legs using : `);
+  animal.superPower.useLaserVision();
+  animal.superPower.beInvisible();
+  console.log('-----------------------------------');
+});
