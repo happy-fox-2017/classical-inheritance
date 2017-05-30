adb8f525321bbb81f5c56026fa88a48440ebba0a
myyusuf <myyusuf1911@gmail.com>
2017-05-30 07:47:47 +0700

    Release 0: A bunch of animals

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,42 @@
 "use strict"
+
+"use strict"
+
+class Animal {
+
+  constructor(numLegs = 2, isWarmBlooded) {
+    this.numLegs = numLegs;
+    this.isWarmBlooded = isWarmBlooded;
+  }
+
+}
+
+class Frog extends Animal {
+  constructor() {
+    super(4, false);
+  }
+}
+
+class Bat extends Animal {
+  constructor() {
+    super(2, true);
+  }
+}
+
+class Chimpanzee extends Animal {
+  constructor() {
+    super(2, true);
+  }
+}
+
+class Fox extends Animal {
+  constructor() {
+    super(4, true);
+  }
+}
+
+class Chicken extends Animal {
+  constructor() {
+    super(2, true);
+  }
+}
